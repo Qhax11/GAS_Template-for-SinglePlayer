69c2042f1a9db5318f9d34966086a9df7759bd30
Şamil <samilozel7@gmail.com>
2025-01-20 18:46:48 +0300

    AI is contiune

---
+++ Source/GAS_TemplateSP/Public/Gameplay/AI/Services/BTService_OrientToTargetActor.h
@@ -0,0 +1,33 @@
+// Qhax's GAS Template for SinglePlayer
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_OrientToTargetActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_TEMPLATESP_API UBTService_OrientToTargetActor : public UBTService
+{
+	GENERATED_BODY()
+
+public:
+
+	UBTService_OrientToTargetActor();
+	
+	// UBTNode Interface
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+	virtual FString GetStaticDescription() const override;
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Target")
+	FBlackboardKeySelector InTargetActorKey;
+
+	UPROPERTY(EditAnywhere, Category = "Target")
+	float RotationInterpSpeed;
+
+};

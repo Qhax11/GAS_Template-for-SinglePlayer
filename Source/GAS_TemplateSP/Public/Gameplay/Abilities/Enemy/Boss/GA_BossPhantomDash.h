489d20ef5126c1ba15e12fcaeed0c8663cccc89d
Şamil <samilozel7@gmail.com>
2025-03-18 23:25:34 +0300

    Boss new ability added

---
+++ Source/GAS_TemplateSP/Public/Gameplay/Abilities/Enemy/Boss/GA_BossPhantomDash.h
@@ -0,0 +1,14 @@
+// Qhax's GAS Template for SinglePlayer
+
+#pragma once
+
+#include "Gameplay/Abilities/Attack/GA_MeleeAttackBase.h"
+#include "GA_BossPhantomDash.generated.h"
+
+
+UCLASS()
+class GAS_TEMPLATESP_API UGA_BossPhantomDashy : public UGA_MeleeAttackBase
+{
+	GENERATED_BODY()
+	
+};

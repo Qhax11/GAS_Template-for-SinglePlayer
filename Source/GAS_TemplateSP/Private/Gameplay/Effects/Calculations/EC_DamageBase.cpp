ee1a6089b5edf3837d76e8316fc5223ab41d3fd8
Şamil <samilozel7@gmail.com>
2025-03-19 16:14:06 +0300

    Added unstoppable attack mechanic

--- Source/GAS_TemplateSP/Private/Gameplay/Effects/Calculations/EC_DamageBase.cpp
+++ Source/GAS_TemplateSP/Private/Gameplay/Effects/Calculations/EC_DamageBase.cpp
@@ -37,7 +37,7 @@ void UEC_DamageBase::ExecuteWithParams(FExecCalculationParameters Params, FGamep
 	const float DamageDealt = CalculateHealth(Params, MitigatedDamage, OutExecutionOutput);
 
 	// Trigger events based on the damage dealt
-	if (DamageDealt > 0)
+	if (DamageDealt > 0 && !Params.TargetASC->HasMatchingGameplayTag(GAS_Tags::TAG_Gameplay_State_InCombat_UnstoppableAttack))
 	{
 		TriggerGameplayEvent(Params, GAS_Tags::TAG_Gameplay_Event_TakeDamage, DamageDealt);
 	}
--- Source/GAS_TemplateSP/Private/Gameplay/Tags/GAS_Tags.cpp
+++ Source/GAS_TemplateSP/Private/Gameplay/Tags/GAS_Tags.cpp
@@ -47,6 +47,7 @@ namespace GAS_Tags
 	UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_State_InCombat_MeleeCombo1, "Gameplay.State.InCombat.MeleeCombo1");
 	UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_State_InCombat_MeleeCombo2, "Gameplay.State.InCombat.MeleeCombo2");
 	UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_State_InCombat_MeleeCombo3, "Gameplay.State.InCombat.MeleeCombo3");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_State_InCombat_UnstoppableAttack, "Gameplay.State.InCombat.UnstoppableAttack");
 
 	UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_State_AbilityTargeting, "Gameplay.State.AbilityTargeting");
 	UE_DEFINE_GAMEPLAY_TAG(TAG_Gameplay_State_AbilityTargeting_Hologram, "Gameplay.State.AbilityTargeting.Hologram");

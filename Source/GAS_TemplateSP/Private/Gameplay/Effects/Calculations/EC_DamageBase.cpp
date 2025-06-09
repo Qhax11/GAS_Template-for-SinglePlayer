7f507feb4a0872bbba1ed541d3af4f3245b75553
Şamil <samilozel7@gmail.com>
2025-06-09 23:49:53 +0300

    Continue

--- Source/GAS_TemplateSP/Private/Gameplay/Effects/Calculations/EC_DamageBase.cpp
+++ Source/GAS_TemplateSP/Private/Gameplay/Effects/Calculations/EC_DamageBase.cpp
@@ -111,6 +111,26 @@ float UEC_DamageBase::CalculateHealth(FExecCalculationParameters& Params, float&
 
 void UEC_DamageBase::TriggerGameplayEvent(FExecCalculationParameters& Params, const FGameplayTag& EventTag, float EventMagnitude) const
 {
+	// Sanity checks
+	if (!Params.TargetASC)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TriggerGameplayEvent: TargetASC is null!"));
+		return;
+	}
+
+	if (!Params.TargetActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TriggerGameplayEvent: TargetActor is null!"));
+		return;
+	}
+
+	if (!EventTag.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("TriggerGameplayEvent: Invalid EventTag passed! Make sure it's registered in the GameplayTags settings: [%s]"), *EventTag.ToString());
+		return;
+	}
+
+	// Prepare payload
 	FGameplayEventData Payload;
 	Payload.EventTag = EventTag;
 	Payload.Instigator = Params.SourceActor;
@@ -118,6 +138,12 @@ void UEC_DamageBase::TriggerGameplayEvent(FExecCalculationParameters& Params, co
 	Payload.ContextHandle = Params.GetSpec().GetContext();
 	Payload.InstigatorTags = Params.GetSpec().CapturedSourceTags.GetActorTags();
 	Payload.EventMagnitude = EventMagnitude;
+
+	// Dispatch the event
+	UE_LOG(LogTemp, Log, TEXT("TriggerGameplayEvent: Sending GameplayEvent [%s] to [%s] with magnitude [%f]"),
+		*EventTag.ToString(),
+		*GetNameSafe(Params.TargetActor),
+		EventMagnitude);
 
 	Params.TargetASC->HandleGameplayEvent(EventTag, &Payload);
 }

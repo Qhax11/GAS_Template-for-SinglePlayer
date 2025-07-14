c24f1c5a89c54c4256469d286adae97438a57b73
Şamil <samilozel7@gmail.com>
2025-07-14 13:00:27 +0300

    Continue

--- Source/GAS_TemplateSP/Public/Gameplay/Components/AC_RespawnBase.h
+++ Source/GAS_TemplateSP/Public/Gameplay/Components/AC_RespawnBase.h
@@ -32,10 +32,10 @@ protected:
 
 	FTimerHandle CharacterDeSpawnCountDownTimerHandle;
 
-	UPROPERTY(EditAnywhere, Category = "RespawnBase")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "RespawnBase")
 	float ReSpawnDelay = 2.0f;
 
-	UPROPERTY(EditAnywhere, Category = "RespawnBase")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "RespawnBase")
 	bool bDisableSpawn = false;
 
 	UPROPERTY(EditDefaultsOnly, Category = "RespawnBase")

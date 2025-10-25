b0dd0f103c69ece5ce7487d7169d541c9ccd500b
Şamil <samilozel7@gmail.com>
2025-10-25 16:12:49 +0300

    Continue

--- Source/GAS_TemplateSP/Private/Gameplay/Abilities/Hero/GA_HeroAirKick.cpp
+++ Source/GAS_TemplateSP/Private/Gameplay/Abilities/Hero/GA_HeroAirKick.cpp
@@ -1,13 +1,18 @@
 // Qhax's GAS Template for SinglePlayer
 
 
-#include "Gameplay/Abilities/Hero/GA_HeroJump.h"
+#include "Gameplay/Abilities/Hero/GA_HeroAirKick.h"
 #include "Gameplay/Abilities/Tracing/GAS_AbilityTraceData.h"
 #include "Gameplay/Effects/GAS_EffectBlueprintFunctionLibary.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemGlobals.h"
 
-void UGA_HeroJump::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+UGA_HeroAirKick::UGA_HeroAirKick()
+{
+	ActivationRequiredTags.AddTag(GAS_Tags::TAG_Gameplay_State_InAir);
+}
+
+void UGA_HeroAirKick::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	const FGameplayAbilityActorInfo* ActorInfo,
 	const FGameplayAbilityActivationInfo ActivationInfo,
 	const FGameplayEventData* TriggerEventData)
@@ -20,14 +25,13 @@ void UGA_HeroJump::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 		return;
 	}
-
+	/*
 	TArray<FHitResult> HitResults;
 	FTraceRequest TraceRequest;
 	TraceData->Trace->CreateTraceWithTeamFilter(GetWorld(), GetAvatarActorFromActorInfo(), ETeamAttitude::Hostile, HitResults, TraceRequest);
 
 	if (!HitResults.IsValidIndex(0)) 
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Detected actor is: %s"), *HitResults[0].GetActor()->GetName());
 		return;
 	}
 
@@ -38,5 +42,5 @@ void UGA_HeroJump::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	UGameplayEffect* GE_Ghost = UGAS_EffectBlueprintFunctionLibary::CreateEffectWithTSubclass(StunEffect);
 
 	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectToTarget(GE_Ghost, TargetASC);
-
+	*/
 }
--- Source/GAS_TemplateSP/Public/Gameplay/Abilities/Hero/GA_HeroAirKick.h
+++ Source/GAS_TemplateSP/Public/Gameplay/Abilities/Hero/GA_HeroAirKick.h
@@ -3,15 +3,17 @@
 #pragma once
 
 #include "Gameplay/Abilities/GA_MontageAbility.h"
-#include "GA_HeroJump.generated.h"
+#include "GA_HeroAirKick.generated.h"
 
 
 UCLASS()
-class GAS_TEMPLATESP_API UGA_HeroJump : public UGA_MontageAbility
+class GAS_TEMPLATESP_API UGA_HeroAirKick : public UGA_MontageAbility
 {
 	GENERATED_BODY()
 	
 public:
+	UGA_HeroAirKick();
+
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData);
 
 	UPROPERTY(EditDefaultsOnly)

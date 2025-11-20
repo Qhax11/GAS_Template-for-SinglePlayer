785bc23a3def6ea9630c8631e83111ebb45f8867
Şamil <samilozel7@gmail.com>
2025-11-20 12:30:20 +0300

    Continue

--- enc_temp_folder/9dee5559a364caa9063557f035a1fd/GA_HeroJump.cpp
+++ enc_temp_folder/9dee5559a364caa9063557f035a1fd/GA_HeroJump.cpp
@@ -11,7 +11,6 @@ UGA_HeroJump::UGA_HeroJump()
 {
 	ActivationBlockedTags.AddTag(GAS_Tags::TAG_Gameplay_State_InAir);
 	ActivationBlockedTags.AddTag(GAS_Tags::TAG_Gameplay_State_InCombat_ParryKnockback);
-	ActivationBlockedTags.AddTag(GAS_Tags::TAG_Gameplay_State_InCombat_Finisher);
 }
 
 void UGA_HeroJump::ActivateAbility(const FGameplayAbilitySpecHandle Handle,

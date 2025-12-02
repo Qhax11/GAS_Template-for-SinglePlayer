a8962e1d732adcab37e726bfffb18f01cb3db290
Şamil <samilozel7@gmail.com>
2025-12-02 14:36:06 +0300

    Continue

---
+++ enc_temp_folder/dbbb3520eea8164d2d32474255c7baf/BDS_ComingAttackReactionBase.h
@@ -0,0 +1,48 @@
+// Qhax's GAS Template for SinglePlayer
+
+#pragma once
+
+#include "Gameplay/AI/BehaviorDecision/Services/BehaviorDecisionServiceBase.h"
+#include "Gameplay/AI/DataTypes/CombatTypes.h"
+#include "Gameplay/AI/BehaviorDecision/Services/ComingAttackReaction/ComingAttackReactionData.h"
+#include "Gameplay/AI/States/Boss/Boss_State_InComingAttack.h"
+#include "BDS_ComingAttackReactionBase.generated.h"
+
+class ComingAttackReactionData;
+
+UCLASS(BlueprintType)
+class UComingAttackReactionAsset : public UPrimaryDataAsset
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TArray<UComingAttackReactionData*> ComingAttackReactions;
+};
+
+UCLASS()
+class GAS_TEMPLATESP_API UBDS_ComingAttackReactionBase : public UBehaviorDecisionServiceBase
+{
+	GENERATED_BODY()
+
+public:
+    virtual void Initialize(const FBehaviorServiceInitParams& BehaviorServiceInitParams) override;
+
+    UComingAttackReactionData* GetBestComingAttackReaction(FComingAttackPayload ComingAttackPayload);
+
+    virtual bool IsEnable(UComingAttackReactionData* ComingReactionData, FComingAttackPayload ComingAttackPayload);
+
+protected:
+    float CalculateBehaviorStateScore(UComingAttackReactionData* ComingReactionData);
+
+    float CalculateTagScore(UComingAttackReactionData* ComingReactionData, const FComingAttackPayload ComingAttackPayload);
+
+    bool PassesFinalChanceRoll(UComingAttackReactionData* ComingReactionData);
+
+    bool PassesChanceRoll(UComingAttackReactionData* ComingReactionData);
+
+    bool PassesChanceRollBasedOnPosture(UComingAttackReactionData* ComingReactionData);
+
+    UPROPERTY(EditDefaultsOnly)
+    UComingAttackReactionAsset* ComingAttackReactionAsset;
+};

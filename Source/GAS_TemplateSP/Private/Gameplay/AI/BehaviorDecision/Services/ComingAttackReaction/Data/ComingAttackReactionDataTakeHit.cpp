647e69788f52ceb3daff6ee8fbe60cf6132e0b84
Şamil <samilozel7@gmail.com>
2025-12-02 18:46:07 +0300

    Continue

---
+++ Source/GAS_TemplateSP/Private/Gameplay/AI/BehaviorDecision/Services/ComingAttackReaction/Data/ComingAttackReactionDataTakeHit.cpp
@@ -0,0 +1,5 @@
+// Qhax's GAS Template for SinglePlayer
+
+
+#include "Gameplay/AI/BehaviorDecision/Services/ComingAttackReaction/Data/ComingAttackReactionDataTakeHit.h"
+
---
+++ Source/GAS_TemplateSP/Public/Gameplay/AI/BehaviorDecision/Services/ComingAttackReaction/Data/ComingAttackReactionDataTakeHit.h
@@ -0,0 +1,17 @@
+// Qhax's GAS Template for SinglePlayer
+
+#pragma once
+
+#include "Gameplay/AI/BehaviorDecision/Services/ComingAttackReaction/Data/ComingAttackReactionData.h"
+#include "ComingAttackReactionDataTakeHit.generated.h"
+
+
+UCLASS()
+class GAS_TEMPLATESP_API UComingAttackReactionDataTakeHit : public UComingAttackReactionData
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool IsEnable(FComingAttackPayload ComingAttackPayload) const override;
+	
+};
